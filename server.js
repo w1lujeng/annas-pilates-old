3d3eea4b1f26572eae1d66ddc574649dd42de97f
geoff dent <gldent@gmail.com>
2017-08-31 19:17:03 -0700

    added server.js

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
+require('dotenv').config();
+require('./config/database');
 
 var app = express();
 
 
 app.use(logger('dev'));
+
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
                            //these are double underscores
+app.use(bodyParser.json());
+
+// API routes 
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/topscores', require('./routes/api/topscores'));
+
+
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -12,10 +12,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Anna's Pilates</h2>
+          <h1>Welcome to Anna's Pilates</h1>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          sign in to see your progress
         </p>
       </div>
     );
